c527b150de0457a35ef9a5d73f0dc0f81b740d7a
myd <1353795848@qq.com>
2019-05-27 23:35:48 +0800

    git commit -m "redis-hot-keys初始化"

---
+++ src/main/java/com/example/redishotkeys/RedisHotKeysApplication.java
@@ -0,0 +1,13 @@
+package com.example.redishotkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisHotKeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisHotKeysApplication.class, args);
+    }
+
+}
